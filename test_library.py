95b373d97fa381d79bbf315dbc07e86d4196c63b
Ilya Turkevich <illya.turkevich@gmail.com>
2023-12-30 21:34:48 +0200

    testing homework

--- test_library.py
+++ test_library.py
@@ -16,18 +16,3 @@ def test_can_convert_negative():
 def test_can_not_convert():
     with pytest.raises(ValueError):
         library.converter(0.03)
-
-
-# def test_can_drive_car():
-#     result = library.can_drive_car(55)
-#     assert result is True
-#
-#
-# def test_not_can_drive_car_min():
-#     with pytest.raises(ValueError):
-#         library.can_drive_car(-555)
-#
-#
-# def test_can_drive_car_max():
-#     with pytest.raises(ValueError):
-#         library.can_drive_car(555)
