7dbf5804585bf668bad9bfca45601d394e75bc71
Ilya Turkevich <illya.turkevich@gmail.com>
2024-01-08 20:27:32 +0200

    CI/CD

--- library.py
+++ library.py
@@ -1,4 +1,4 @@
 def converter(cm: int) -> float:
     if type(cm) is float:
-        raise ValueError('Can only convert integer')
+        raise ValueError("Can only convert integer")
     return round(cm * 0.3937008, 1)
