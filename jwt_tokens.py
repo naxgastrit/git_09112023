2dad510cb341e00f5f0a7e367abe8cca2f91aed3
Ilya Turkevich <illya.turkevich@gmail.com>
2024-01-11 22:53:24 +0200

    hashing homework

--- jwt_tokens.py
+++ jwt_tokens.py
@@ -17,12 +17,28 @@ encode_jwt = jwt.encode(
 )
 print(encode_jwt)
 
-decoded = jwt.decode(
-    encode_jwt,
-    settings.JWT_SECRET,
-    algorithms=["HS256"],
-    # options={
-    #     'verify_signature': False
-    # },
-)
-print(decoded)
+
+def encode_jwt(payload: dict) -> str:
+    return jwt.encode(
+        payload=payload,
+        key=settings.JWT_SECRET,
+        algorithm="HS256",
+    )
+
+
+def decoded_jwt(encoded_jwt_str: str) -> dict:
+    try:
+        return jwt.decode(
+            encoded_jwt_str,
+            settings.JWT_SECRET,
+            algorithms=["HS256"],
+        )
+    except jwt.exceptions.ExpiredSignatureError:
+        return {}
+    except jwt.exceptions.InvalidSignatureError:
+        return {}
+
+
+data = encode_jwt(payload)
+
+result = decoded_jwt(data)
